09693f6d788829e361248f5d2c53768ae92b01af
NoEnd-yt <landaubros.yt@gmail.com>
2021-03-21 10:32:16 +0100

    Modified If/Elif/Else conditions

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-#coding=<'utf-8'>
+#coding=<"utf-8">
 from sys import argv
+debug = False
 
 # Variable
 variables = {}
@@ -7,22 +8,22 @@ variables = {}
 # Opens file
 if len(argv) > 1:
 	fileToOpen = argv[1]
-	file = open(fileToOpen,mode='r')
+	file = open(fileToOpen,mode="r")
 	lines = file.readlines()
-	print(lines)
+	if debug == True:print(lines)
 
 # LEXER
 
 # Lexer functions
 def makeInt(content):
-	if content[3] == 'MATH':
+	if content[3] == "MATH":
 		mathOutput = content.copy()
 		del mathOutput[:3]
 		content[3] = makeMath(mathOutput)
 		varValue = int(content[3])
 		varName = content[1]
 		variables[varName] = varValue
-	elif content[3] == 'INPUT':
+	elif content[3] == "INPUT":
 		output = content.copy()
 		del output[:3]
 		content[3] = xenonInput(output)
@@ -35,14 +36,14 @@ def makeInt(content):
 		variables[varName] = varValue
 
 def makeFloat(content):
-	if content[3] == 'MATH':
+	if content[3] == "MATH":
 		mathOutput = content.copy()
 		del mathOutput[:3]
 		content[3] = makeMath(mathOutput)
 		varValue = float(content[3])
 		varName = content[1]
 		variables[varName] = varValue
-	elif content[3] == 'INPUT':
+	elif content[3] == "INPUT":
 		output = content.copy()
 		del output[:3]
 		content[3] = xenonInput(output)
@@ -55,14 +56,14 @@ def makeFloat(content):
 		variables[varName] = varValue
 
 def makeStr(content):
-	if content[3] == 'MATH':
+	if content[3] == "MATH":
 		mathOutput = content.copy()
 		del mathOutput[:3]
 		content[3] = makeMath(mathOutput)
 		varValue = str(content[3])
 		varName = content[1]
 		variables[varName] = varValue
-	elif content[3] == 'INPUT':
+	elif content[3] == "INPUT":
 		output = content.copy()
 		del output[:3]
 		content[3] = xenonInput(output)
@@ -75,17 +76,17 @@ def makeStr(content):
 		variables[varName] = varValue
 
 def makeBool(content):
-	if content[3].lower() == 'true':
+	if content[3].lower() == "true":
 		varValue = True
-	elif content[3].lower() == 'false':
+	elif content[3].lower() == "false":
 		varValue = False
 	varName = content[1]
 	variables[varName] = varValue
 
 def addition(content):
-	if '+' in content[1]:
-		nums = content[1].split('+')
-		print(nums)
+	if "+" in content[1]:
+		nums = content[1].split("+")
+		if debug == True:print(nums)
 		keys = list(variables.keys())
 		for i in range(len(nums)):
 			if nums[i] in keys:
@@ -97,12 +98,12 @@ def addition(content):
 			result += nums[i+1]
 		return result
 	else:
-		return 'failure'
+		return "failure"
 
 def substraction(content):
-	if '-' in content[1]:
-		nums = content[1].split('-')
-		print(nums)
+	if "-" in content[1]:
+		nums = content[1].split("-")
+		if debug == True:print(nums)
 		keys = list(variables.keys())
 		for i in range(len(nums)):
 			if nums[i] in keys:
@@ -114,12 +115,12 @@ def substraction(content):
 			result -= nums[i+1]
 		return result
 	else:
-		return 'failure'
+		return "failure"
 
 def multiplication(content):
-	if '*' in content[1]:
-		nums = content[1].split('*')
-		print(nums)
+	if "*" in content[1]:
+		nums = content[1].split("*")
+		if debug == True:print(nums)
 		keys = list(variables.keys())
 		for i in range(len(nums)):
 			if nums[i] in keys:
@@ -131,12 +132,12 @@ def multiplication(content):
 			result *= nums[i+1]
 		return result
 	else:
-		return 'failure'
+		return "failure"
 
 def division(content):
-	if '/' in content[1]:
-		nums = content[1].split('/')
-		print(nums)
+	if "/" in content[1]:
+		nums = content[1].split("/")
+		if debug == True:print(nums)
 		keys = list(variables.keys())
 		for i in range(len(nums)):
 			if nums[i] in keys:
@@ -148,28 +149,28 @@ def division(content):
 			result /= nums[i+1]
 		return result
 	else:
-		return 'failure'
+		return "failure"
 
 def makeMath(content):
 	result = addition(content)
-	if result == 'failure':
+	if result == "failure":
 		result = substraction(content)
-		if result == 'failure':
+		if result == "failure":
 			result = multiplication(content)
-			if result == 'failure':
+			if result == "failure":
 				result = division(content)
 	return result
 
 def xenonPrint(content):
 	keys = list(variables.keys())
-	output = ''
-	if content[1] == 'MATH':
+	output = ""
+	if content[1] == "MATH":
 		mathOutput = content.copy()
 		del mathOutput[:1]
 		content[1] = makeMath(mathOutput)
 		output = content[1]
-	elif '#' in content[1]:
-		stringInput = content[1].split('#')
+	elif "#" in content[1]:
+		stringInput = content[1].split("#")
 		for i in range(len(keys)):
 			for x in range(len(stringInput)):
 				if keys[i] == stringInput[x]:
@@ -185,44 +186,85 @@ def xenonPrint(content):
 
 def xenonInput(content):
 	stringInput = content[1:]
-	output = ''
+	output = ""
 	for i in range(len(stringInput)):
 		output += str(stringInput[i])
 	result = input(output)
 	return result
 
+def xenonIf(content):
+	keys = list(variables.keys())
+	for i in range(len(content)):
+		for x in range(len(keys)):
+			if content[i] == keys[x]:
+				content[i] = variables[keys[x]]
+	if content[2] == "==":
+		if content[1] == content[3]:
+			result = True
+		else:
+			result = False
+	elif content[2] == "!=":
+		if content[1] != content[3]:
+			result = True
+		else:
+			result = False
+	if result == True:
+		return 0
+	else:
+		return 1
+
 # Lexer main loop
+ifIfCondition = 0
 for i in range(len(lines)):
-	if '§' in lines[i]:
-		codeLineStr = lines[i].split('§')
-		if '\n' in codeLineStr:
-			lineReturnIndex = codeLineStr.index('\n')
+	if "<<" in lines[i]:
+		codeLineStr = lines[i].split("<<")
+		if "\n" in codeLineStr:
+			lineReturnIndex = codeLineStr.index("\n")
 			del codeLineStr[lineReturnIndex]
-		codeLine = codeLineStr[0].split(' ')
-		while '' in codeLine:
-			spaceIndex = codeLine.index('')
+		codeLine = codeLineStr[0].split(" ")
+		while "" in codeLine:
+			spaceIndex = codeLine.index("")
 			del codeLine[spaceIndex]
 		codeLineStr2 = codeLineStr[1:]
 		for i in range(len(codeLineStr2)):
 			codeLine.append(codeLineStr2[i])
-		print(codeLine)
+		if debug == True:print(codeLine)
 	else:
-		codeLine = lines[i].split(' ')
-		codeLine[-1] = codeLine[-1].rstrip('\n')
-		print(codeLine)
-	if codeLine[0] == 'INT':
-		makeInt(codeLine)
-		print(variables)
-	elif codeLine[0] == 'FLOAT':
-		makeFloat(codeLine)
-		print(variables)
-	elif codeLine[0] == 'STR':
-		makeStr(codeLine)
-		print(variables)
-	elif codeLine[0] == 'BOOL':
-		makeBool(codeLine)
-		print(variables)
-	elif codeLine[0] == 'MATH':
-		print(makeMath(codeLine))
-	elif codeLine[0] == 'PRINT':
-		xenonPrint(codeLine)
+		codeLine = lines[i].split(" ")
+		codeLine[-1] = codeLine[-1].rstrip("\n")
+		if debug == True:print(codeLine)
+	if "\t" in codeLine[0]:
+		codeLine[0] = codeLine[0].strip("\t")
+	if ifIfCondition == 0:
+		if codeLine[0] == "INT":
+			makeInt(codeLine)
+			if debug == True:print(variables)
+		elif codeLine[0] == "FLOAT":
+			makeFloat(codeLine)
+			if debug == True:print(variables)
+		elif codeLine[0] == "STR":
+			makeStr(codeLine)
+			if debug == True:print(variables)
+		elif codeLine[0] == "BOOL":
+			makeBool(codeLine)
+			if debug == True:print(variables)
+		elif codeLine[0] == "MATH":
+			if debug == True:print(makeMath(codeLine))
+		elif codeLine[0] == "PRINT":
+			xenonPrint(codeLine)
+	if codeLine[0] == "IF":
+		ifIfCondition = xenonIf(codeLine)
+	elif codeLine[0] == "ELIF":
+		if ifIfCondition == 1:
+			ifIfCondition = 0
+			ifIfCondition = xenonIf(codeLine)
+		elif ifIfCondition == 0:
+			ifIfCondition = 2
+	elif codeLine[0] == "ELSE":
+		if ifIfCondition == 1:
+			ifIfCondition = 0
+		else:
+			ifIfCondition = 1
+	elif codeLine[0] == "END":
+		ifIfCondition = 0
+	if debug == True:print("ifIfCondition:",ifIfCondition)
